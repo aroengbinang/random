84bbe65e05be41daf9d1e71058128977bf131b1f
aroengbinang <65850876+aroengbinang@users.noreply.github.com>
2020-06-10 19:00:03 +0700

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,27 @@
+//<![CDATA[
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+
+ 
+function onYouTubeIframeAPIReady() {
+player = new YT.Player('player', {
+width: '660',
+height: '370',
+events: {
+'onReady': onPlayerReady
+},
+playerVars: {
+listType: 'playlist',
+list: 'PL1EB10B19837A1B61'
+}
+});
+}
+function onPlayerReady(event) {
+num = Math.floor(Math.random() * 100);
+setTimeout(() => {
+player.playVideoAt(num);
+}, 1000);
+} //]]>
