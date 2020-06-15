5911175c5c20168661ece9844afad19de0b83cb6
aroengbinang <65850876+aroengbinang@users.noreply.github.com>
2020-06-15 17:30:41 +0700

    Update youtube_stop.js

--- youtube_stop.js
+++ youtube_stop.js
@@ -30,7 +30,7 @@ player.playVideoAt(num);
 var done = false;
 function onPlayerStateChange(event) {
 if (event.data == YT.PlayerState.PLAYING && !done) {
-setTimeout(stopVideo, 10000);
+setTimeout(stopVideo, 1000);
 done = true;
 }
 }
